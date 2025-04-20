088cec6cdf9b0ad52a91a95b3ed733d5f2e82971
ksamrat224 <ksamrat224@gmail.com>
2025-04-20 20:20:35 +0545

    form validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Counter from './components/Counter'
+import UserInfo from './components/UserInfo'
 const App = () => {
   return (
     <>
     <div className="flex flex-wrap justify-center gap-10 mt-10">
-        <Counter/>
+        <UserInfo/>
       </div>
    </>
   )
